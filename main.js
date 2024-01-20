6dee2b973b54b667fdef6567c87d7885995aaf4b
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-01-20 21:52:36 +0200

    add

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import "./src/App/index.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./components/App/App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="tester">
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
