be63f6c4408f8a8021503e7196c0f2e2ca731bdc
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-01-20 22:18:26 +0200

    try to

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: "pre", ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+  ],
 });
